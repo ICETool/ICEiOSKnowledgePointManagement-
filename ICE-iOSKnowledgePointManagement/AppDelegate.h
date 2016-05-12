0bc00eeffaf69111a2753a25cff869a6f10490fb
gumengxiao <rare_ice@163.com>
2016-05-12 09:32:48 +0800

    Initial Commit

---
+++ ICE-iOSKnowledgePointManagement/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ICE-iOSKnowledgePointManagement
+//
+//  Created by WLY on 16/5/12.
+//  Copyright © 2016年 ICE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
